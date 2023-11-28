6e9a596ee2b9ba253b3c562debe8d1fd67b97e30
Riccardo Mollo <riccardmollo84@gmail.com>
2023-11-28 11:43:36 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RetroPixelizer
-RetroPixelizer is a simple tool that converts a square image to an old-style monochrome pixelated image.
+
+**RetroPixelizer** is a simple tool that converts a square image to an old-style monochrome pixelated image.
+
+**Preview:**
+
+<a href="https://ibb.co/n71ybvZ"><img src="https://i.ibb.co/hBdNyjh/in.jpg" alt="in" border="0"></a>
+<a href="https://ibb.co/mhX5Fdm"><img src="https://i.ibb.co/L9JdhBM/out.jpg" alt="out" border="0"></a>
---
+++ retropixelizer.py
@@ -0,0 +1,79 @@
+import argparse
+import os
+import sys
+from PIL import Image, ImageEnhance
+from termcolor import colored
+
+
+def error(message):
+    print(colored("ERROR!", "red", attrs=["reverse", "bold"]) + " " + message)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="retropixelizer.py",
+        description="RetroPixelizer is a simple tool that converts a square image to an old-style monochrome pixelated image.",
+    )
+    parser.add_argument("-i", "--infile", type=str, help="input image", required=True)
+    parser.add_argument("-o", "--outfile", type=str, help="output image", required=True)
+    parser.add_argument(
+        "-s",
+        "--size",
+        type=str,
+        help="side size in pixel (default: 64)",
+        default=64,
+        required=False,
+    )
+    parser.add_argument(
+        "-c",
+        "--colors",
+        type=int,
+        help="colors (default: 8)",
+        default=8,
+        required=False,
+    )
+    parser.add_argument(
+        "-f",
+        "--contrast-factor",
+        type=float,
+        help="contrast factor (default: 1.5)",
+        default=1.5,
+        required=False,
+    )
+    args = parser.parse_args()
+    in_file = args.infile
+    out_file = args.outfile
+    size = args.size
+    colors = args.colors
+    factor = args.contrast_factor
+
+    if not os.path.isfile(in_file):
+        error('File "' + in_file + '" does not exist or is not readable.')
+        sys.exit(1)
+
+    if not os.path.isfile(in_file):
+        error('File "' + in_file + '" does not exist or is not readable.')
+        sys.exit(1)
+
+    input_image = Image.open(in_file)
+    width, height = input_image.size
+
+    if width != height:
+        error("The image must be square (ratio 1:1).")
+        sys.exit(1)
+
+    output_image = input_image.resize((size, size), resample=Image.BILINEAR).resize(
+        input_image.size, Image.NEAREST
+    )
+
+    output_image = output_image.convert("RGB")
+    contrast_factor = factor
+    enhancer = ImageEnhance.Contrast(output_image)
+    output_image = enhancer.enhance(contrast_factor)
+    output_image = output_image.quantize(colors=colors)
+    output_image = output_image.convert("L")
+    output_image.save(out_file, optimize=False)
+
+
+if __name__ == "__main__":
+    main()
